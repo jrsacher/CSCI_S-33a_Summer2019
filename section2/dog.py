3384a0d085f67fbcea7a52a2f8166be9f0e46b10
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2019-07-02 21:55:11 -0400

    Added weight and conditional barking

--- section2/dog.py
+++ section2/dog.py
@@ -1,12 +1,16 @@
 from my_class import Dog
 
-fido = Dog("golden")
+fido = Dog("golden", 65)
 print(fido.breed)
 print(fido.good)
 fido.bark()
 print()
 
-pepper = Dog("pug", False)
+pepper = Dog("pug", 20, False)
 print(pepper.breed)
 print(pepper.good)
 pepper.bark()
+print()
+
+scraps = Dog("chihuahua", 4)
+scraps.bark()
