8b8273726477afa74eaeae2386b4aa7665be338c
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2019-07-02 21:54:28 -0400

    Added weight and conditional barking

--- section2/my_class.py
+++ section2/my_class.py
@@ -1,9 +1,14 @@
 class Dog:
     # Initialize the class
-    def __init__(self, breed, good=True):
+    def __init__(self, breed, weight, good=True):
         self.breed = breed
+        self.weight = weight
         self.good = good
-    
+            
     # Speak!
     def bark(self):
-        print("ruff")
+        if self.weight < 10:
+            print("yip")
+        else:
+            print("ruff")
+            
