b4543744f321f53b2ebd2a9cefa5a07bc8c259c6
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2019-07-09 18:50:26 -0400

    Create models.py

---
+++ section4/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Dog(db.Model):
+    __tablename__ = "dogs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    breed = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    def add_owner(self, name):
+        person = Owner(name=name, dog_id=dogs.id)
+        db.session.add(person)
+        db.session.commit()
+
+
+class Owner(db.Model):
+    __tablename__ = "owners"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    dog_id = db.Column(db.Integer, db.ForeignKey("dogs.id"), nullable=False)
+    
