4c81bf239b1f4f244260ccfc2c4c89a89853e9dc
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2019-07-09 18:50:47 -0400

    Create create.py

---
+++ section4/create.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://xbadxprxmkrvbx:815c4c0eb89edf45b25763444505cc5bccf63c0248a730f325c290054c06af21@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d2q23hivule7au'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
+        
